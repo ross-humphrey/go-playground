762c3bccfc15313065c420d9385ff42a97402d8c
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-08-14 09:25:53 +0100

    Adding switch statement file to demonstrate how switches work in Go. File also demonstrates string formatting and different print types.

---
+++ switch.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	// example of a basic switch statement
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// You can use commas to separate multiple expressions in the same case statement
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch without an expression is an alternate way to express if/else logic
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// A type switch compares types rather than values
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know the type %T\n", t) // example of formatted print
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+
+}
