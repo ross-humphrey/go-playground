f32675999216b9e4890243ed878a3961b70c7542
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-08-14 07:13:11 +0100

    added constants example

---
+++ constants.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "math"
+
+const s string = "constant"
+
+// working with constants
+func main() {
+	fmt.Println(s)
+
+	// n is not assigned a type here
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	// but n is given a type in context
+	fmt.Println(math.Sin(n))
+	
+}
