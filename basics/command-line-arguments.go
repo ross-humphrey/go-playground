1f54535a0d2efbc38c59c0c426f558ac18585c77
candyflossuk <ross.a.humphrey@googlemail.com>
2019-09-13 00:19:40 +0100

    🔫 Added command line arguments example

---
+++ basics/command-line-arguments.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+Command line arguments are a common way to parameterize execution of programs.
+*/
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// os.Args provides access to raw command-line arguments
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[:1]
+
+	// get individual args with normal indexing
+	arg := os.Args[3]
+
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+
+}
